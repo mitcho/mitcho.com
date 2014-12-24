afeeddd1bc882045f6e82b50e2b75f3fd42e5f00
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2014-12-24 04:39:58 -0500

    build overlay files

--- _plugins/papers.rb
+++ _plugins/papers.rb
@@ -9,6 +9,29 @@ module Jekyll
       
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'paper.html')
+      self.data['title'] = bibdata['title']
+      self.data['paper'] = bibdata
+    end
+
+    # Override so that we can control where the destination file goes
+    def destination(dest)
+      # The url needs to be unescaped in order to preserve the correct filename.
+      path = File.join(dest, @dir, @name )
+#       path = File.join(path, "index.html") if self.url =~ /\/$/
+      path
+    end     
+  end
+
+  class PaperOverlayPage < Page
+    def initialize(site, base, dir, slug, bibdata)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = slug + '.overlay.html'
+      
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'overlay-paper.html')
+      self.data['title'] = bibdata['title']
       self.data['paper'] = bibdata
     end
 
@@ -30,6 +53,7 @@ module Jekyll
         site.data['bib'].each do |bibdata|
           slug = Utils.slugify(bibdata['citationKey'])
           site.pages << PaperPage.new(site, site.source, dir, slug, bibdata)
+          site.pages << PaperOverlayPage.new(site, site.source, dir, slug, bibdata)
         end
       end
     end
