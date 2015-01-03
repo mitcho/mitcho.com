a8952e4cbb40aaa353e2a48d42fff09ac6eff4bb
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2015-01-03 15:36:07 -0600

    tweak deploy

--- _deploy.sh
+++ _deploy.sh
@@ -2,10 +2,11 @@
 # deploy script
 
 echo "Deploying to server..."
-rsync -vrzc --delete _site/ root@mitcho.com:/var/www/mitcho.com/html
+rsync -rzcp --chmod=a+r --delete _site/ root@mitcho.com:/var/www/mitcho.com/html
 echo "Deploy complete!"
 
 echo "Updating redirects file"
-rsync -vzc _site/redirects.conf root@mitcho.com:/var/www/mitcho.com/conf/redirects.conf
+rsync -zcp --chmod=a+r _site/redirects.conf root@mitcho.com:/var/www/mitcho.com/conf/redirects.conf
 echo "Kicking nginx"
+ssh root@mitcho.com 'service nginx configtest'
 ssh root@mitcho.com 'service nginx reload'
